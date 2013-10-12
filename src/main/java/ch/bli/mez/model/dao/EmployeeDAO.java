a46095777bac03fdfbf9aa841b5c4d0022e1e93a
Michael Brodmann <brodmann@bl-informatik.ch>
2013-10-12 15:48:16 +0200

    * added persistens to employeeDAO * added javadoc support of maven. usage: add these goals javadoc:javadoc site:site

--- src/main/java/ch/bli/mez/model/dao/EmployeeDAO.java
+++ src/main/java/ch/bli/mez/model/dao/EmployeeDAO.java
@@ -12,38 +12,42 @@ public class EmployeeDAO {
 
   private SessionFactory sessionFactory;
   private Session session;
-  
+
   public EmployeeDAO() {
     sessionFactory = SessionManager.getSessionFactory();
     session = sessionFactory.openSession();
   }
 
   public List<Employee> findAll() {
-	List<Employee> employees = session.createQuery("from Employee").list();
-	session.flush();
+    List<Employee> employees = session.createQuery("from Employee").list();
+    session.flush();
     return employees;
   }
 
   public void addEmployee(Employee employee) {
-    session.save(employee);
-    session.flush();
+    Transaction tx = session.beginTransaction();
+    try {
+      session.save(employee);
+      session.flush();
+      tx.commit();
+    } catch (Exception ex) {
+      tx.rollback();
+    }
   }
-  
+
   public Employee getEmployee(Integer id) {
-	  Employee employee = (Employee) session.load(
-		        Employee.class, id);
-	  session.flush();
-	  return employee;
+    Employee employee = (Employee) session.load(Employee.class, id);
+    session.flush();
+    return employee;
   }
-  
+
   public void updateEmployee(Employee employee) {
-	  session.update(employee);
-	  session.flush();
+    session.update(employee);
+    session.flush();
   }
 
   public void deleteEmployee(Integer id) {
-    Employee employee = (Employee) session.load(
-        Employee.class, id);
+    Employee employee = (Employee) session.load(Employee.class, id);
     if (null != employee) {
       session.delete(employee);
     }
