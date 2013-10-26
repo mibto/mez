ce33a94489f2edf84e1c5ac332852d01f11ab4aa
avanthay <dave@avanthay.ch>
2013-10-26 11:20:07 +0200

    create empty PositionController

---
+++ src/main/java/ch/bli/mez/controller/PositionController.java
@@ -0,0 +1,19 @@
+package ch.bli.mez.controller;
+
+import ch.bli.mez.view.PositionPanel;
+
+public class PositionController {
+	
+	private PositionPanel view;
+//	private PositionDAO model;
+	
+	public PositionController(){
+		this.view = new PositionPanel();
+//		this.model = new PositionDAO();
+	}
+	
+	public PositionPanel getView(){
+		return view;
+	}
+
+}
