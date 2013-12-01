31f9e0ea607bfa0fac16069cbc4e0d758e46cfbc
dave <dave@avanthay.ch>
2013-12-01 13:21:55 +0100

    create DefaultPanel and DefaultForm

---
+++ src/main/java/ch/bli/mez/view/DefaultForm.java
@@ -0,0 +1,15 @@
+package ch.bli.mez.view;
+
+import javax.swing.JPanel;
+
+public abstract class DefaultForm extends JPanel {
+
+  private static final long serialVersionUID = -4757726896100150339L;
+
+  public Boolean validateFields(){
+    return null;
+  }
+  
+  public void showAsCreateNew(){}
+  
+}
