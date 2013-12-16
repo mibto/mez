4fc4845b43daf3e8b8ec346b2510487bc0e2a839
Michael Brodmann <brodmann@bl-informatik.ch>
2013-12-16 14:27:19 +0100

    removed package info file generated ul li list save html content to file and start browser

--- src/main/java/ch/bli/mez/controller/FormatMissionReportController.java
+++ src/main/java/ch/bli/mez/controller/FormatMissionReportController.java
@@ -1,8 +1,12 @@
 package ch.bli.mez.controller;
 
+import java.awt.Desktop;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
-import ch.bli.mez.model.TimeEntry;
 import ch.bli.mez.util.TimeEntriesPerEmployee;
 import ch.bli.mez.util.TimeEntriesPerMission;
 import ch.bli.mez.util.TimeEntriesPerPosition;
@@ -10,18 +14,67 @@ import ch.bli.mez.util.TimeEntriesPerPosition;
 public class FormatMissionReportController {
 
   public void formatTimeEntries(List<TimeEntriesPerMission> timeEntriesPerMissions) {
-    // TODO Auto-generated method stub
-    for (TimeEntriesPerMission timeEntriesPerMission: timeEntriesPerMissions){
-      System.out.println(timeEntriesPerMission.getMission().getMissionName() + " " + timeEntriesPerMission.getTotalTime());      
-      for (TimeEntriesPerPosition timeEntriesPerPosition: timeEntriesPerMission.getTimeEntriesPerPositions()){
-        System.out.println("    " + timeEntriesPerPosition.getPosition().getPositionName() + " " + timeEntriesPerPosition.getTotalTime());      
-        if (timeEntriesPerPosition.getShowEmployees()){
-          for (TimeEntriesPerEmployee timeEntriesPerEmployee: timeEntriesPerPosition.getTimeEntriesPerEmployees()){
-            System.out.println("        " + timeEntriesPerEmployee.getEmployee().getLastName() + " " + timeEntriesPerEmployee.getTotalTime());      
-          }          
-        }
-      }
-    }
+    showReport(createReport(timeEntriesPerMissions));
   }
 
+  private String createReport(List<TimeEntriesPerMission> timeEntriesPerMissions) {
+    String report = "<!Doctype html>";
+    report += "<ul>";
+    for (TimeEntriesPerMission timeEntryPerMission : timeEntriesPerMissions) {
+      report += "<li>";
+      report += "<span class='missionName'>" + timeEntryPerMission.getMission().getMissionName() + "</span>";
+      report += "<span class='missionTotal'>" + timeEntryPerMission.getTotalTime() + "</span>";
+      report += "<ul>";
+
+      for (TimeEntriesPerPosition timeEntryPerPosition : timeEntryPerMission.getTimeEntriesPerPositions()) {
+        report += "<li>";
+        report += "<span>" + timeEntryPerPosition.getPosition().getPositionName() + "</span>";
+        report += "<span>" + timeEntryPerPosition.getTotalTime() + "</span>";
+        if (timeEntryPerPosition.getShowEmployees()) {
+          report += "<ul>";
+          for (TimeEntriesPerEmployee timeEntriesPerEmployee : timeEntryPerPosition.getTimeEntriesPerEmployees()) {
+            report += "<li>";
+            report += "<span>" + timeEntriesPerEmployee.getEmployee().getLastName() + "</span>";
+            report += "<span>" + timeEntriesPerEmployee.getTotalTime() + "</span>";
+            report += "</li>";
+          }
+          report += "</ul>";
+        } else {
+          report += "</li>";
+        }
+        report += "</li>";
+      }
+      report += "</ul>";
+      report += "</li>";
+    }
+    report += "</ul>";
+    return report;
+  }
+
+  private void showReport(String report) {
+    File reportFile;
+    try {
+      reportFile = File.createTempFile("report", ".html");
+      BufferedWriter out = new BufferedWriter(new FileWriter(reportFile));
+      out.write(report);
+      out.close();
+      if (Desktop.isDesktopSupported()) {
+        Desktop desktop = Desktop.getDesktop();
+        try {
+          desktop.browse(reportFile.toURI());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      } else {
+        Runtime runtime = Runtime.getRuntime();
+        try {
+          runtime.exec("xdg-open " + reportFile.toURI());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
