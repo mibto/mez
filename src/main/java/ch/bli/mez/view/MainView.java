96ad15b66887e977bd80e9ba3cdb415b29584988
Stephan Zumbuehl <stephan.zumbuehl@gmail.com>
2014-01-30 18:29:31 +0100

    Glass-Layout

--- src/main/java/ch/bli/mez/view/MainView.java
+++ src/main/java/ch/bli/mez/view/MainView.java
@@ -39,8 +39,8 @@ public class MainView extends JFrame {
     
     try {
       UIManager
-//       .setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
-          .setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+       .setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
+ //         .setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
     } catch (Exception e) {
       e.printStackTrace();
     }
